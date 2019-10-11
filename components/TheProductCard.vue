b3330ae6e5557963f2ea95f976d9cfc00c81cdef
Rony Lopez <ronylopez@Ronys-MacBook-Pro.local>
2019-10-11 17:25:34 -0600

    Demo site v1

--- components/TheProductCard.vue
+++ components/TheProductCard.vue
@@ -1,27 +1,18 @@
-<template>
+<template style="margin: 2em">
   <v-card max-width="344">
     <v-list-item>
       <v-list-item-avatar color="grey"></v-list-item-avatar>
       <v-list-item-content>
-        <v-list-item-title class="headline"
-          >Our Changing Planet</v-list-item-title
-        >
-        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+        <v-list-item-title class="headline">{{ header }}</v-list-item-title>
+        <v-list-item-subtitle>by {{ author }}</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
     <v-img :src="imageLink" height="194"></v-img>
     <v-card-text>
-      Visit ten places on our planet that are undergoing the biggest changes
-      today.
+      {{ header }}
     </v-card-text>
 
     <v-card-actions>
-      <v-btn text color="deep-purple accent-4">
-        Read
-      </v-btn>
-      <v-btn text color="deep-purple accent-4">
-        Bookmark
-      </v-btn>
       <div class="flex-grow-1"></div>
       <v-btn icon>
         <v-icon>mdi-eco</v-icon>
@@ -37,14 +28,17 @@
 export default {
   name: 'TheProductCard',
   props: {
+    header: {
+      type: String,
+      default: ''
+    },
     imageLink: {
       type: String,
       default: ''
-    }
-  },
-  computed: {
-    transformedUrl() {
-      return ''
+    },
+    author: {
+      type: String,
+      default: ''
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,39 @@
     <v-layout row wrap>
       <the-product-card
         image-link="https://picsum.photos/id/248/3872/2592"
+        style="margin: 1em"
+        header="Cactus"
+        author="Viverito"
       ></the-product-card>
       <the-product-card
         image-link="https://picsum.photos/id/1/5616/3744"
+        style="margin: 1em"
+        header="Servicios de programacion"
+        author="Allium"
       ></the-product-card>
       <the-product-card
         image-link="https://picsum.photos/id/1025/4951/3301"
+        style="margin: 1em"
+        header="Se vende perro"
+        author="Adoptame"
+      ></the-product-card>
+      <the-product-card
+        image-link="https://picsum.photos/id/152/3888/2592"
+        style="margin: 1em"
+        header="Arreglo de flores"
+        author="Viverito"
+      ></the-product-card>
+      <the-product-card
+        image-link="https://picsum.photos/id/1080/6858/4574"
+        style="margin: 1em"
+        header="Fresas a granel"
+        author="Eres Fresa"
+      ></the-product-card>
+      <the-product-card
+        image-link="https://picsum.photos/id/111/4400/2656"
+        style="margin: 1em"
+        header="Automovil"
+        author="Pedro Perez"
       ></the-product-card>
     </v-layout>
   </v-container>
